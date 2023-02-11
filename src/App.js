91f9da55c3ff90934c4dfa6b4e0fcb5e2db3d8bc
Bilal Ahmed <bilalahmedim@gmail.com>
2023-02-11 15:22:31 +0530

    About Page Structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './Components/Pages/HomePage';
+import AboutPage from './Components/Pages/AboutPage';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> }
+  { path: '/', element: <HomePage /> },
+  { path: '/about-us', element: <AboutPage /> }
 ]);
 
 
