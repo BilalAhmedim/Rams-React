29d8908f0d542fa9578fbea5f2badf50207d22f9
Bilal Ahmed <bilalahmedim@gmail.com>
2023-02-14 17:10:07 +0530

    postcss setup complete

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = (ctx) => ({
+  map: ctx.options.map,
+  plugins: [
+    // require("sourcemap"), 
+    require("postcss-import"),
+    require("postcss-nested"),
+    require("postcss-mixins"),
+    require("postcss-simple-vars"),
+    require("postcss-preset-env"),
+    require("postcss-discard-comments"),
+    require("autoprefixer"),
+    // require("cssnano"), 
+  ],
+});
