6b9444cb04039a417f90c6d4d0eb593ae937aed8
Bilal Ahmed <bilalahmedim@gmail.com>
2023-03-14 12:12:55 +0530

    basic card tweaks. Changes to be committed: 	modified:   src/Components/UI/Card.css 	modified:   src/Components/UI/Gallery.js 	modified:   src/Components/UI/Global.css 	modified:   src/Components/postcss/Card.css 	modified:   src/Components/postcss/Global.css

--- src/Components/UI/Gallery.js
+++ src/Components/UI/Gallery.js
@@ -8,7 +8,7 @@ const Gallery = (props) => {
   return (
     <div className='gallery'>
       <Header header={props.header} />
-      <div className='wrapper'>
+      <div className='wrapper clearfix'>
         <Card title='Fire Pit' path='/' image={thumb} />
         <Card title='Fire Pit' path='/' image={thumb} />
         <Card title='Fire Pit' path='/' image={thumb} />
