80424b31b2498d2734a6d5c57f963b17d97441a1
Bilal Ahmed <bilalahmedim@gmail.com>
2023-03-20 12:15:36 +0530

    about us text formatting Changes to be committed: 	modified:   src/Components/Pages/AboutPage.js 	modified:   src/Components/UI/Global.css 	modified:   src/Components/postcss/Global.css

--- src/Components/Pages/AboutPage.js
+++ src/Components/Pages/AboutPage.js
@@ -8,85 +8,94 @@ const AboutPage = () => {
     <React.Fragment>
       <Navigation />
       <Header header='About Us' />
-      <div className="about wrapper">
-        <h1>History</h1>
+      <div className="about wrapper--fixed">
 
-        <p>
-          Rams International is a leading Manufacturers exporter of Copper Bath Tubs, Wash Basins & Kitchen Sinks (Copper & Stain less steel). Rams International, established in 2003, it has been only over the last 16 years that we have experienced the tremendous change that has brought us to where we are today.
-        </p>
+        <section>
+          <h1>History</h1>
+          <p>
+            Rams International is a leading Manufacturers exporter of Copper Bath Tubs, Wash Basins & Kitchen Sinks (Copper & Stain less steel). Rams International, established in 2003, it has been only over the last 16 years that we have experienced the tremendous change that has brought us to where we are today.
+          </p>
 
-        <p>
-          Rams International, maintains strong working ethics and relationships with its Clients. This gives the company control over the products it handles, and the ability to react quickly and efficiently to customers&#8217; changing needs.
-        </p>
+          <p>
+            Rams International, maintains strong working ethics and relationships with its Clients. This gives the company control over the products it handles, and the ability to react quickly and efficiently to customers&#8217; changing needs.
+          </p>
+        </section>
 
-        <h1>Team</h1>
+        <section>
+          <h1>Team</h1>
+          <p>
+            Rams International is a proprietor ship concern. Mr. Riyaz Anwer is a founder and mentor. He also supervises the manufacturing & R & D section. He deals with every day challenges of production manufacturing technique. Mr. Riyaz Anwer is considered the only man to introduce Copper Bathtubs & Kitchen Sinks in Moradabad. He is technically expert.
+          </p>
+          <p>
+            Mr. Riyaz Anwer has an extensive background in the Copper bath tubs & Kitchen sinks etc. He has a keen eye for design and practical knowledge. Riyaz Anwer has inherited this manufacturing skill and ability from his ancestors who have been recognized as peerless leaders in their field for many generations. 150 artisans work under his direct supervision. The beauty of our company lies in the unique expression of each item. Our designers have great expertise in their skill and they put their heart into every item created.
+          </p>
+        </section>
 
-        <p>
-          Rams International is a proprietor ship concern. Mr. Riyaz Anwer is a founder and mentor. He also supervises the manufacturing & R & D section. He deals with every day challenges of production manufacturing technique. Mr. Riyaz Anwer is considered the only man to introduce Copper Bathtubs & Kitchen Sinks in Moradabad. He is technically expert.
-        </p>
+        <section>
+          <h1>Fabrication Facility</h1>
+          <p>
+            Rams International produces quality copper and manufactures copper products in it&#8217;s own fabrication facility. Most sink suppliers purchase their products from a third party, having no control over the manufacturing process. We have a covered area of 25000 Sq. FT. We use only durable & heavier sheets in making our product. Normally, we use copper sheets in 16 gauges with copper contents 99.95%. The copper we use is only the finest of solid copper and our sinks are completely food-safe i.e. we also provide Lab testing report verifying the genuineness of copper.
+          </p>
+        </section>
 
-        <p>
-          Mr. Riyaz Anwer has an extensive background in the Copper bath tubs & Kitchen sinks etc. He has a keen eye for design and practical knowledge. Riyaz Anwer has inherited this manufacturing skill and ability from his ancestors who have been recognized as peerless leaders in their field for many generations. 150 artisans work under his direct supervision. The beauty of our company lies in the unique expression of each item. Our designers have great expertise in their skill and they put their heart into every item created.
-        </p>
+        <section>
+          <h1>Our Support</h1>
+          <p>
+            Rams International produces sinks/tubs with European Standards in place. The sinks are specifically engineered to work with Standardized European plumbing. Our entire line of products has been developed with you in mind. We support our customers with quality products, Timely shipment and reasonable rates. We develop products as per your confidential designs, specifications and requirements with our well-equipped manufacturing unit.
+          </p>
+        </section>
 
-        <h1>Fabrication Facility</h1>
+        <section>
+          <h1>Finishes</h1>
+          <p>
+            We make our product in different finishes as; Shinny polished, Copper Antique, Patinas- Old French patina, Nickel, Pewter and Tin-plated.
+          </p>
+        </section>
 
-        <p>
-          Rams International produces quality copper and manufactures copper products in it&#8217;s own fabrication facility. Most sink suppliers purchase their products from a third party, having no control over the manufacturing process. We have a covered area of 25000 Sq. FT. We use only durable & heavier sheets in making our product. Normally, we use copper sheets in 16 gauges with copper contents 99.95%. The copper we use is only the finest of solid copper and our sinks are completely food-safe i.e. we also provide Lab testing report verifying the genuineness of copper.
-        </p>
+        <section>
+          <h1>Quality Control</h1>
+          <p>We do not compromise on quality because it is the prime factor. To save our customers from receiving poor quality or poor specification goods, we maintain strict quality control during manufacturing of our products. Every item is carefully inspected before it is shipped. Therefore, our buyers receive the quality that they need. We take the responsibility for damaged or breakage of the goods in the form of replacement.
+          </p>
+        </section>
 
-        <h1>Our Support</h1>
+        <section>
+          <h1>On line Inspection</h1>
+          <p>
+            We provide on line inspection on the items that we pack.
+          </p>
+        </section>
 
-        <p>
-          Rams International produces sinks/tubs with European Standards in place. The sinks are specifically engineered to work with Standardized European plumbing. Our entire line of products has been developed with you in mind. We support our customers with quality products, Timely shipment and reasonable rates. We develop products as per your confidential designs, specifications and requirements with our well-equipped manufacturing unit.
-        </p>
+        <section>
+          <h1>Packing</h1>
+          <p>
+            Our packing is very exclusive having the norms of UPS packaging.
+          </p>
+          <p>
+            We use polythene bags + bubbler + PU Foam. Thence we put it into strong plywood case. We pack the Tubs/Sinks inside the cases in such a manner that movement inside the cases is impossible. The inside of case is “lined” with various materials e.g. stout & damp resisting paper etc. to prevent the damage by water, air, or insects. In addition, the goods is wrapped & generously padded to avoid scratching and knocking against the cases.
+          </p>
 
-        <h1>Finishes</h1>
+          <p>
+            Market: North America & Europe
+          </p>
 
-        <p>
-          We make our product in different finishes as; Shinny polished, Copper Antique, Patinas- Old French patina, Nickel, Pewter and Tin-plated.
-        </p>
+          <p>
+            <b>Warranty:</b>  Our products are warranted from manufacturer defects (leaking/rusting etc.) for 5 years.
+          </p>
 
-        <h1>Quality Control</h1>
+          <p>
+            <b>Delivery Time:</b>  We take maximum 45 days for making the volume worth the capacity of 40&#8217; feet container.
+          </p>
 
-        <p>We do not compromise on quality because it is the prime factor. To save our customers from receiving poor quality or poor specification goods, we maintain strict quality control during manufacturing of our products. Every item is carefully inspected before it is shipped. Therefore, our buyers receive the quality that they need. We take the responsibility for damaged or breakage of the goods in the form of replacement.
-        </p>
+          <p>
+            <b>Payment:</b>  We execute the business on 50% as advance and 50% on B/L OR against irrevocable L/C.
+          </p>
 
-        <h1>On line Inspection</h1>
+          <p>
+            <b>AN ISO CERTIFIED COMPANY:</b>  ISO 9001:2015, ISO 14001:2015, OHSAS 18001:2007 & SEDEX 2 PILLER AUDITED
+          </p>
+        </section>
 
-        <p>
-          We provide on line inspection on the items that we pack.
-        </p>
 
-        <h1>Packing</h1>
-
-        <p>
-          Our packing is very exclusive having the norms of UPS packaging.
-        </p>
-
-        <p>
-          We use polythene bags + bubbler + PU Foam. Thence we put it into strong plywood case. We pack the Tubs/Sinks inside the cases in such a manner that movement inside the cases is impossible. The inside of case is “lined” with various materials e.g. stout & damp resisting paper etc. to prevent the damage by water, air, or insects. In addition, the goods is wrapped & generously padded to avoid scratching and knocking against the cases.
-        </p>
-
-        <p>
-          Market: North America & Europe
-        </p>
-
-        <p>
-          <b>Warranty:</b>  Our products are warranted from manufacturer defects (leaking/rusting etc.) for 5 years.
-        </p>
-
-        <p>
-          <b>Delivery Time:</b>  We take maximum 45 days for making the volume worth the capacity of 40&#8217; feet container.
-        </p>
-
-        <p>
-          <b>Payment:</b>  We execute the business on 50% as advance and 50% on B/L OR against irrevocable L/C.
-        </p>
-
-        <p>
-          <b>AN ISO CERTIFIED COMPANY:</b>  ISO 9001:2015, ISO 14001:2015, OHSAS 18001:2007 & SEDEX 2 PILLER AUDITED
-        </p>
 
       </div>
       <Footer />
