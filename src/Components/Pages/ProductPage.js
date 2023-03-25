400303257e64dafeb569ad6e3559ad2734d8edba
Bilal Ahmed <bilalahmedim@gmail.com>
2023-03-25 15:09:21 +0530

    product page added only. Changes to be committed: 	modified:   src/Components/Pages/ProductPage.js 	new file:   src/Components/UI/ProductView.css 	new file:   src/Components/UI/ProductView.js 	new file:   src/Components/assets/Rams ER.mwb 	new file:   src/Components/postcss/ProductView.css

--- src/Components/Pages/ProductPage.js
+++ src/Components/Pages/ProductPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navigation from './../UI/Navigation'
 import Footer from './../UI/Footer';
 import Header from '../UI/Header';
+import ProductView from '../UI/ProductView';
 import './../UI/Page.css'
 
 const ProductPage = () => {
   return (
     <React.Fragment>
       <Navigation />
+      <ProductView />
       <Header header='Suggestions' />
       <Footer />
     </React.Fragment>
---
+++ src/Components/UI/ProductView.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './ProductView.css'
+
+
+const ProductView = () => {
+  return (
+    <React.Fragment>
+      <div className='product-view'>
+        <div className='wrapper-fixed'>
+
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+
+
+export default ProductView;
