98253924c824263763d10533b53e1ef536259db3
Bilal Ahmed <bilalahmedim@gmail.com>
2023-03-27 14:52:17 +0530

    product view basic stucture and styling. Changes to be committed: 	modified:   src/Components/UI/ProductView.css 	modified:   src/Components/UI/ProductView.js 	new file:   src/Components/assets/1_front.JPG 	new file:   src/Components/assets/1_iso.JPG 	new file:   src/Components/assets/1_top.JPG 	modified:   src/Components/postcss/ProductView.css

--- src/Components/UI/ProductView.js
+++ src/Components/UI/ProductView.js
@@ -1,13 +1,65 @@
 import React from 'react'
 import './ProductView.css'
+import image_iso from './../assets/1_iso.JPG';
+import image_front from './../assets/1_front.JPG';
+import image_top from './../assets/1_top.JPG';
 
 
 const ProductView = () => {
+
+  const product =
+  {
+    name: '72” Brass Double Slipper Bath tub',
+    item_num: 'RA72DB-BL/PL',
+    finish: 'Black Outside / Polish Inside',
+    material: 'Brass 16 Gauge',
+    size: '72”L X 32”W X 31”H',
+    avail_finish: 'Antique, Mate, Polish, Others',
+    tags: ' Brass, Double Slipper, bath tub',
+    certification: 'UPC',
+    material_options: 'Brass, Copper, Steel'
+  }
+
+
   return (
     <React.Fragment>
       <div className='product-view'>
         <div className='wrapper-fixed'>
+          <div className='product-view__container'>
 
+            <div className='product-view__light-box'>
+
+              <img src={image_iso} alt="Product Large" />
+
+              <div className='arrow'>
+                <div className='arrow__right'>&nbsp;</div>
+                <div className='arrow__left'>&nbsp;</div>
+              </div>
+
+              <ul>
+                <li><img src={image_iso} alt='First View' /></li>
+                <li><img src={image_front} alt='Second View' /></li>
+                <li><img src={image_top} alt='Third View' /></li>
+                <li><img src={image_front} alt='Fourth View' /></li>
+              </ul>
+
+            </div>
+
+            <div className='product-view__info'>
+              <ul>
+                <li>Product Name: {product.name}</li>
+                <li>Product Item: {product.item_num}</li>
+                <li>finsh: {product.finish}</li>
+                <li>material: {product.material}</li>
+                <li>size: {product.size}</li>
+                <li>finish available: {product.avail_finish}</li>
+                <li>tags: {product.tags}</li>
+                <li>crtifications: {product.certification}</li>
+                <li>material options: {product.material_options}</li>
+              </ul>
+            </div>
+
+          </div>
         </div>
       </div>
     </React.Fragment>
